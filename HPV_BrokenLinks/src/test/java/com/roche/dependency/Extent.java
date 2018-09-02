53669335fbc9f11883b485541fd258fc2f229570
debanbhattachar3 <debanbhattachar3@USHYDEBANBHATT5.us.deloitte.com>
2018-09-02 20:24:50 +0530

    Extent class Update

--- HPV_BrokenLinks/src/test/java/com/roche/dependency/Extent.java
+++ HPV_BrokenLinks/src/test/java/com/roche/dependency/Extent.java
@@ -5,7 +5,7 @@ import com.relevantcodes.extentreports.ExtentReports;
 public class Extent {
 	public static ExtentReports getInstance(){
 		ExtentReports extent;
-		String path = System.getProperty("user.dir") + "\\target\\Extent_Report.html";
+		String path = System.getProperty("user.dir") + "\\test-output\\Extent_Report.html";
 		extent = new ExtentReports(path, true);//, DisplayOrder.NEWEST_FIRST);
 		extent.addSystemInfo("Browser", "Chrome");
 		extent.addSystemInfo("Platform", "Windows");
